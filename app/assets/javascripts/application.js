97fd8215e913c94be0da5e2d3a718c6f12fa1a71
vagrant <vagrant@localhost.localdomain>
2021-01-20 05:45:04 +0000

    [Add]トップ画面
    [Add]Skippr

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,19 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function () {
+  $("#images").skippr({
+    transition : 'slide',
+    speed : 1000,
+    easing : 'easeOutQuart',
+    navType : 'block',
+    childrenElementType : 'div',
+    arrows : true,
+    autoPlay : true,
+    autoPlayDuration : 3000,
+    keyboardOnAlways : true,
+    hidePrevious : false
+  });
+});
+
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def top
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
