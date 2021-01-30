d4eb5de69afc736d0bf78a408f9cda2afdc3c94f
vagrant <vagrant@localhost.localdomain>
2021-01-30 07:53:19 +0000

    [Update]画像表示

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ebd13a42f339232485d8373cd78a3563c9d41f083868759f20ff3b52bf33bcf495d0152722ab70ac2eba7dd6b2a5ecc5ad737df7792ea402aa371ac67006c070'
